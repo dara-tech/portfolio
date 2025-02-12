43467219ab45899726f67c94163ec76a3ab3739b
Dara <97618988+dara-tech@users.noreply.github.com>
2025-02-12 20:26:40 +0700

    r

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "/";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "https://daracheol.onrender.com";
 
 
 const useAuth = () => {
