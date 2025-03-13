445d7ee07e83489f6f3dae3aafd99a91d4e418e1
Dara <97618988+dara-tech@users.noreply.github.com>
2025-03-13 09:27:02 +0700

    server

--- frontend/src/hooks/useRoadMap.js
+++ frontend/src/hooks/useRoadMap.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5002' : 'https://daracheol.onrender.com';
+const API_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5002' : 'https://daracheol.onrender.com/';
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
